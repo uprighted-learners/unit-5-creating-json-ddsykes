00d8f531c6cfea2191566f49a0514032f1353937
ddsykes <ddsykes@hotmail.com>
2024-07-06 19:23:17 -0500

    adding lab

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+//const PORT = 3000;
+
+app.get("/answer", (req, res) => {
+  res.sendFile(path.join(__dirname, "api", "answers.json"));
+});
 
 app.listen(PORT, () => {
-    console.log(`Listening on PORT ${PORT}`);
+  console.log(`Listening on PORT ${PORT}`);
 });
